8d1db77828ac70baf3ed8bee818f7720b6fdfaa1
Marta Koperek <makoperek@gmail.com>
2021-08-24 22:32:09 +0200

    Change styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './styles/global.scss';
-import './styles/custom-btsp.scss'
-
 import App from './components/App/App';
 
 ReactDOM.render(
